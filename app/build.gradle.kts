dc71caa6e03c5c300fd7f23cb868d93d6f25205a
Kmiit <kmiit@foxmail.com>
2026-04-02 19:51:44 +0800

    Bump miuix to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
     }
 
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
